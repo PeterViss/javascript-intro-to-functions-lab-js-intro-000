6aee125b1be2ef6b3ab2645289a15cfd1e0ce9ab
stupendous-trainer-5541 <peter.c.viss@biola.edu>
2019-01-27 06:02:34 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,27 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log() string.toUpperCase
+}
+
+function logWhisperer(string) {
+  console.log string.toLowerCase
+}
+
+function sayHiToGrandma(string) {
+  if((string)===uppercase) {
+    return 'Yes INDEED!'
+  }
+  else if ((string)===lowercase) {
+    return "I can'/t hear you!"
+  }
+  }
+
+
+}
